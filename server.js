eccd19a706f105ad8de1bff48d0b6e9125394c7a
Adrian Lee <alee.lcagency@gmail.com>
2018-03-13 20:58:55 +0800

    pushing to server

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(function (err, req, res, next) {
 });
 
 // express server listening to port 8000
-const port = 8000;
+const port = 8080;
 
 app.listen(port, () => {
     console.log('Server is running at port ' + port);
